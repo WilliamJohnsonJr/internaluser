f1339e017297e5923e14e93a640b97499bba9c7b
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-06-28 17:14:44 -0400

    Wrote main.js to import jQuery and inject nodes into HTML with appropriate values from API dataset. Styled in SASS in main.scss. Added blur effect by using text-shadow and making text transparent.

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,31 @@ apiData.then(function(apiData){
 	var dataArray = apiData.results;
 
 	dataArray.forEach(function(object){
-		
+		$(".grid").append(`
+			<div class="userContainer">
+				<div class="imgContainer ${object.name.last}">
+					<img class="gridImg" src="${object.picture.large}">
+				</div>
+				<div class="nameContainer">
+					<span class="name">${object.name.first} ${object.name.last}</span>
+				</div>
+				<div class="emailContainer">
+				${object.email}
+				</div>
+				<div class="address1Container">
+				${object.location.street}
+				</div>
+				<div class="address2Container">
+				${object.location.city}, ${object.location.state}, ${object.location.postcode}
+				</div>
+				<div class="phoneNumber">
+				${object.phone}
+				</div>
+				<div class="SSN">
+				${object.id.value}
+				</div>
+			</div>
+			`)
 	})
 	
 });
